a7911f05164e22c56b8e2f9fd5f447a071346aa6
Walquiria Godoy <walquiriagodoy@gmail.com>
2021-09-09 15:02:47 -0300

    Hello World simples eliminando componentes desnecessários do Create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button } from '@material-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Laboratório <code>da</code> Wal
-        </p>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/laboratoriodeforro/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Página do instagram
-        </a>
-      </header>
-    </div>
-  );
+  return <Button color='primary'>Hello World</Button>;
 }
-
+  
 export default App;
