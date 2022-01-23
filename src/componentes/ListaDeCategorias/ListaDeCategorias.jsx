d09bf762d838102e5fc18125fd2ad3bfb29e693d
Walquiria Godoy <walquiriagodoy@gmail.com>
2022-01-23 11:07:05 -0300

    feat(css): Add css to category list

--- src/componentes/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/componentes/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./estilo.css";
 class ListaDeCategorias extends Component {
     state = {  } 
 
@@ -11,7 +12,7 @@ class ListaDeCategorias extends Component {
 
     render() { 
         return (
-            <section>
+            <section className='lista-categorias'>
                 <ul className='lista-categorias_lista'>
                     {this.props.categorias.map((categoria, index)=>{
                         return(<li key={index} className='lista-categorias_item'>{categoria}</li>)
